f7c14dec0299ebef4b917e0b0dcfdd8efeb93eba
woaimadaima <2914115192@qq.com>
2022-05-12 21:40:44 +0800

    完成了AST的Json生成

--- src/visualization.cpp
+++ src/visualization.cpp
@@ -1,21 +1,67 @@
 /**
  * @file visualization.cpp
  * @brief define the AST's visulization using json tools
- * @author Wang Hui (3190105609@zju.edu.cn)
- * @version 1.0
- * @date 2022-05-11
+ * @author Wang Hui & Xu Zhengtao 
+ * @version 2.0
+ * @date 2022-05-12
  * @copyright Copyright (c) 2022
  * 
  */
-#include"ast.h"
+#include "ast.h"
+#include "type.h"
 
 /**
  * @brief 
  * exlclude jsonGen() from ast.cpp
  * @return Json::Value 
- * modification log: 2022/5/11,19:49
- * modificated by: Wang Hui
+ * modification log: 2022/5/12,21:39
+ * modificated by: Xu Zhengtao
  */
+
+
 Json::Value Node::jsonGen(){
-    //TODO
+    Json::Value root;
+    string addstr = "";
+    if(this->node_Type == "Typer" || this->node_Type == "Exp"){
+        switch (this->getValueType())
+        {
+        case VOID:
+            addstr = "void"; break;
+        case VAR:
+            addstr = "var"; break;
+        case ARRAY:
+            addstr = "array"; break;
+        case FUN:
+            addstr = "function"; break;
+        case TYPE_INT:
+            addstr = "int"; break;
+        case TYPE_INT_ARRAY:
+            addstr = "int_Array"; break;
+        case TYPE_FLOAT:
+            addstr = "float"; break;
+        case TYPE_FLOAT_ARRAY:
+            addstr = "float_Array"; break;
+        case TYPE_CHAR:
+            addstr = "char"; break;
+        case TYPE_CHAR_ARRAY:
+            addstr = "char_Array"; break;
+        case TYPE_DOUBLE:
+            addstr = "double"; break;
+        case TYPE_DOUBLE_ARRAY:
+            addstr = "double_Array"; break;
+        case POINTER:
+            addstr = "pointer"; break;
+        default:
+            break;
+        }
+    }
+
+    root["name"] = *this->node_Type + (addstr == "" ? "" : ": " + addstr);
+
+    for(int i = 0; i < this->child_Num; i++){
+        if(this->child_Node[i]){
+            root["children"].append(this->child_Node[]->jsonGen());
+        }
+    }
+    return root;
 }
