3957172caa96b69061fd291765a273b5ad780079
xwxr <zju_xwx@zju.edu.cn>
2022-05-21 23:14:05 +0800

    delete include

--- test/matrix-multiplication.c
+++ test/matrix-multiplication.c
@@ -2,7 +2,7 @@
  * @Author: xwxr
  * @Date: 2022-05-14 10:19:46
  * @LastEditors: xwxr
- * @LastEditTime: 2022-05-15 18:04:44
+ * @LastEditTime: 2022-05-21 22:57:11
  * @Description: delete addition assignment operator '+='
  */
 
@@ -14,8 +14,6 @@
  * @Description: "matrix multiplication" test for C-Minus.
  */
 
-#include <stdio.h>
-
 int main()
 {
     int m1, n1, m2, n2;
@@ -23,36 +21,54 @@ int main()
     int i, j;
     int A[25][25], B[25][25];
     int num;
-    for (i = 0; i < m1; i++) {
-        for (j = 0; j < n1; j++) {
+    i = 0;
+    while (i < m1) {
+        j = 0;
+        while (j < n1) {
             scanf("%6d", &num);
             A[i][j] = num;
+            j++;
         }
+        i++;
     }
     scanf("%d %d", &m2, &n2);
-    for (i = 0; i < m2; i++) {
-        for (j = 0; j < n2; j++) {
+    i = 0;
+    while (i < m2) {
+        j = 0;
+        while (j < n2) {
             scanf("%6d", &num);
             B[i][j] = num;
+            j++;
         }
+        i++;
     }
     if (n1 != m2) {
         printf("Incompatible Dimensions\n");
     } else {
         int C[25][25], k;
-        for (i = 0; i < m1; i++) {
-            for (j = 0; j < n2; j++) {
+        i = 0;
+        while (i < m1) {
+            j = 0;
+            while (j < n2) {
                 C[i][j] = 0;
-                for (k = 0; k < n1; k++) {
+                k = 0;
+                while (k < n1) {
                     C[i][j] = C[i][j] + A[i][k] * B[k][j];
+                    k++;
                 }
+                j++;
             }
+            i++;
         }
-        for (i = 0; i < m1; i++) {
-            for (j = 0; j < n2; j++) {
+        i = 0;
+        while (i < m1) {
+            j = 0;
+            while (j < n2) {
                 printf("%10d", C[i][j]);
+                j++;
             }
             printf("\n");
+            i++;
         }
     }
 
